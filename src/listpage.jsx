baa30e0188ba6548cca082d4afd03ce2e8cbc63b
khushi-dhand <156648647+khushi-dhand@users.noreply.github.com>
2024-10-18 10:21:09 +0530

    Update listpage.jsx

--- src/listpage.jsx
+++ src/listpage.jsx
@@ -1,7 +1,24 @@
+import { listData } from "./dummydata";
+import "./listpage.scss"
+import Filter from "./filter.jsx"
+import Card from "./card.jsx"
+
 function ListPage(){
+    const data =listData;
     return (
-        <div className="listpage">listpage</div>
-    )
+        <div className="list-page">
+        <div className="listcontainer">
+            <div className="wrapper">
+                <Filter/>
+                {data.map(item=>(
+                    <Card key={item.id} item={item}/>
+                ))}
+            </div>
+
+        </div>
+        <div className="mapcontainer">map</div>
+        </div>
+    );
 }
 
 export default ListPage
